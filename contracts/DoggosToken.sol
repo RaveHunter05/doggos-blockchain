30c2a4840054706bb193710bcc4629bce20550d9
exra2mp <exra2mp@gmail.com>
2025-02-09 08:00:25 -0600

    DoggosToken Ready for final team revision

--- contracts/DoggosToken.sol
+++ contracts/DoggosToken.sol
@@ -45,7 +45,8 @@ contract DoggosToken is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Pausab
     //     string addressRef; // String representation of the address of the owner.
     // }
 
-    mapping(uint256 => Pet) petList;
+    mapping(uint256 => string[]) public petLogs;
+    mapping(uint256 => Pet) public petList;
     // mapping(address => Owner) ownerList;
     mapping(uint256 => address) public petOwnership;
     mapping(uint256 => address) public petToVeterinary;
@@ -53,6 +54,9 @@ contract DoggosToken is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Pausab
     event PetSafeMinted(uint256 tokenId, string name, address owner);
     event OwnerInfoAdded(address ownerAddress, string name, uint256 age, string sex, string locationAddress, string country, string telephone, uint256 badge);
     event DogTransferred(uint256 tokenId, address from, address to);
+    event LogAdded(uint256 tokenId, string logType, string description, uint256 timestamp); 
+    event PetRemoved(uint256 tokenId, address owner);
+    event PetTransferred(uint256 indexed tokenId, address indexed from, address indexed to);
 
     constructor(address _defaultAdmin) ERC721("DoggosToken", "DOGT") {
         _grantRole(DEFAULT_ADMIN_ROLE, _defaultAdmin); // Only set the default admin in constructor
@@ -93,8 +97,7 @@ contract DoggosToken is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Pausab
         string memory sex, 
         string memory birthdate, 
         uint256[] memory vaccinesList, 
-        uint256[] memory surgeries, 
-        string memory status
+        uint256[] memory surgeries
     ) public onlyRole(MINTER_ROLE) {
         uint256 tokenId = _nextTokenId++;
         _safeMint(owner, tokenId);
@@ -110,12 +113,21 @@ contract DoggosToken is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Pausab
             birthdate: birthdate,
             vaccinesList: vaccinesList,
             surgeries: surgeries,
-            status: status,
+            status: "LIVING",
             owner: owner,
             veterinaryAddressRef: address(0)
         });
 
         petOwnership[tokenId] = owner;
+
+         // Log the initial minting.  Use an event for efficient off-chain indexing.
+        _addLog(tokenId, "MINT", "Pet minted", block.timestamp);
+        emit PetSafeMinted(tokenId, name, owner);
+    }
+
+    function _addLog(uint256 tokenId, string memory logType, string memory description, uint256 timestamp) internal {
+        petLogs[tokenId].push(string(abi.encodePacked(logType, "|", description, "|", Strings.toString(timestamp))));
+        emit LogAdded(tokenId, logType, description, timestamp);
     }
 
     // The following functions are overrides required by Solidity.
@@ -133,12 +145,25 @@ contract DoggosToken is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Pausab
     function setPetInSurgery(uint256 tokenId) public onlyRole(DEFAULT_ADMIN_ROLE) {
         require(petList[tokenId].veterinaryAddressRef == msg.sender, "You are not the owner of this dog");
         petList[tokenId].status = "IN_SURGERY";
+         _addLog(tokenId, "SURGERY", "Pet put in surgery", block.timestamp); // Log the surgery
     }
 
     function unassignVeterinary(uint256 tokenId) public onlyRole (DEFAULT_ADMIN_ROLE) {
         petList[tokenId].veterinaryAddressRef = address(0);
+        _addLog(tokenId, "VET_UNASSIGNED", "Veterinary unassigned", block.timestamp);
     }
 
+    function getPetLogs(uint256 tokenId)public view returns (string[] memory logs){
+        require(petList[tokenId].owner != address(0), "pet has not been minted yet");
+        return petLogs[tokenId];
+    }
+
+    function getPetInfo(uint256 tokenId) public view returns (Pet memory) {
+    require(petOwnership[tokenId] == msg.sender, "You are not the owner of this pet");
+    return petList[tokenId];
+    }
+
+   
     function _increaseBalance(address account, uint128 value)
         internal
         override(ERC721, ERC721Enumerable)
@@ -163,4 +188,121 @@ contract DoggosToken is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Pausab
     {
         return super.supportsInterface(interfaceId);
     }
+
+    function removePet(uint256 tokenId) public onlyRole(DEFAULT_ADMIN_ROLE) {  
+        require(_ownerOf(tokenId) != address(0), "Pet does not exist"); 
+        address owner = petOwnership[tokenId];
+        delete petOwnership[tokenId];
+        delete petLogs[tokenId]; //Check if need something else
+        _burn(tokenId);
+        emit PetRemoved(tokenId, owner);
+    }
+
+    function updatePetInfo (
+        uint256 tokenId,
+        string memory newName,
+        string memory newBreed,
+        string memory newColor, 
+        string memory newSex,
+        string memory newBirthDate, 
+        uint256 newAge,
+        uint256 newWeight,  /* Frontend should multiply by 100 i.e. 4550 (45.50 * 100) */
+        uint256[] memory newVaccinesList, /// Frontend should has JSON list of this
+        uint256[] memory newSurgeries
+        ) public onlyRole (DEFAULT_ADMIN_ROLE){
+            require(_ownerOf(tokenId) != address(0), "Pet does not exist"); // Check if the owner owns a pet, otherwise no need to update info 
+             petList[tokenId].name = newName;
+             petList[tokenId].breed = newBreed;
+             petList[tokenId].color = newColor;
+             petList[tokenId].age = newAge;
+             petList[tokenId].sex = newSex; //TODO: Frontend will have this option. 
+             petList[tokenId].birthdate = newBirthDate;
+             petList[tokenId].weight = newWeight /100 ;/// Frontend should multiply by 100 i.e. 4550 (45.50 * 100)  
+             petList[tokenId].vaccinesList = newVaccinesList;// Check if the new vaccines list is empty or not?
+             petList[tokenId].surgeries = newSurgeries;
+    
+        _addLog(tokenId, "UPDATE_PET", "Pet info updated", block.timestamp);
+        }
+
+    bytes16 private constant _HEX_SYMBOLS = "0123456789abcdef";
+
+    function _addressToString(address addr) internal pure returns (string memory) {
+    bytes memory str = new bytes(42);
+    str[0] = '0';
+    str[1] = 'x';
+    for (uint i = 0; i < 20; i++) {
+        uint8 b = uint8(uint256(uint160(addr)) / (2**(4*(19 - i))) & 0xf); // Convertir b a uint8
+        bytes1 upper = bytes1(b >> 4); // b ya es uint8, no necesita conversión
+        bytes1 lower = bytes1(b & uint8(0xf)); // b ya es uint8, no necesita conversión
+        str[2+i*2] = _HEX_SYMBOLS[uint8(upper)];
+        str[3+i*2] = _HEX_SYMBOLS[uint8(lower)];
+    }
+    return string(str);
+    }
+
+    function transferPet(uint256 tokenId, address newOwner) public {
+    require(_ownerOf(tokenId) == msg.sender, "You are not the owner of this pet");
+
+    address previousOwner = _ownerOf(tokenId);
+
+    transferFrom(msg.sender, newOwner, tokenId);
+
+    _addLog(tokenId,
+        "TRANSFER",
+        string(abi.encodePacked("Pet transferred to ", _addressToString(newOwner))), // Concatenación correcta
+        block.timestamp); // Reutilizar _addLog
+
+    emit PetTransferred(tokenId, previousOwner, newOwner);
+    }
+
+    function getVaccinesList(uint256 tokenId) public view returns (uint256[] memory) {
+    require(_ownerOf(tokenId) != address(0), "Pet does not exist");
+    require(petOwnership[tokenId] == msg.sender, "You are not the owner of this pet");
+    return petList[tokenId].vaccinesList;
+    }
+
+    function getPetsByOwner(address owner) public view returns (Pet[] memory) {
+    Pet[] memory petArray = new Pet[](0); // start an empty array
+    uint256 petCount = 0;
+    uint256 totalTokens = totalSupply();
+
+    for (uint256 i = 0; i < totalTokens; i++) {
+        uint256 tokenId = tokenByIndex(i);
+        if (_ownerOf(tokenId) == owner) {
+            petCount++;
+            Pet[] memory temp = new Pet[](petCount);
+            for (uint256 j = 0; j < petCount - 1; j++) {
+                temp[j] = petArray[j];
+            }
+            temp[petCount - 1] = petList[tokenId]; // Asign struct Pet directly
+            petArray = temp;
+        }
+    }
+
+    return petArray;
+   }
+
 }
+
+ library Strings {
+        bytes16 private constant _HEX_SYMBOLS = "0123456789abcdef";
+
+        function toString(uint256 value) internal pure returns (string memory) {
+            if (value == 0) {
+                return "0";
+            }
+            uint256 temp = value;
+            uint256 digits;
+            while (temp != 0) {
+                digits++;
+                temp /= 10;
+            }
+            bytes memory buffer = new bytes(digits);
+            while (value != 0) {
+                digits -= 1;
+                buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
+                value /= 10;
+            }
+            return string(buffer);
+        }
+    }
